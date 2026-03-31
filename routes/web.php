f431ceb35d124131f48ca3bc207a373ac2d0facb
imjazzdev <jazzdev01@gmail.com>
2026-03-31 17:02:22 +0700

    refactor: refactor routes

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CheckinController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\RecapController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\ClubController;
+
 Route::get('/', function () {
     return view('coming-soon');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware([\App\Http\Middleware\ApiAuthenticated::class])->group(function () {
+    Route::get('/checkin', [CheckinController::class, 'index'])->name('checkin');
+    Route::post('/checkin', [CheckinController::class, 'checkIn']);
+    Route::post('/checkout', [CheckinController::class, 'checkOut']);
+
+    Route::get('/members', [MemberController::class, 'index'])->name('members');
+    Route::post('/members', [MemberController::class, 'store']);
+    Route::get('/members/{id}', [MemberController::class, 'show']);
+
+    Route::get('/recap', [RecapController::class, 'index'])->name('recap');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('users', UserController::class)->except(['create','edit','show']);
+        Route::resource('clubs', ClubController::class)->except(['create','edit','show']);
+    });
 });
-
-Route::get('/change-password', function () {
-    return view('auth.change-password');
-});
-
-Route::get('/checkin', function () {
-    return view('checkin');
-});
-
-Route::get('/members', function () {
-    return view('members.index');
-});
-
-Route::get('/recap', function () {
-    return view('recap.index');
-});
-
-Route::prefix('admin')->group(function () {
-    Route::get('/users', function () {
-            return view('admin.users');
-        }
-        );
-
-        Route::get('/clubs', function () {
-            return view('admin.clubs');
-        }
-        );    });
